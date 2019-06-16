33f5aa1b4d1d16d82deedb8c1ea6412bb365efb7
Your Name <you@example.com>
2019-06-16 17:10:51 +0300

    Moar code

--- multirotor_dynamics.py
+++ multirotor_dynamics.py
@@ -25,6 +25,30 @@
 
 import numpy as np
 
+class MultirotorDynamics(object):
+
+    # Might want to allow G to vary based on altitude
+    g  = 9.80665 
+
+    def __init(self, motorCount, b, d, m, l, Ix, Iy, Iz, Jr, maxrpm):
+
+        self.motorCount = motorCount
+
+        self._x = np.zeros(12)
+
+        self.omegas = np.zeros(motorCount)
+
+        self.b = b
+        self.d = d
+        self.m = m
+        self.l = l
+        self.Ix = Ix
+        self.Iy = Iy
+        self.Iz = Iz
+        self.Jr = Jr
+
+        self.maxrpm = maxrpm
+
 '''
 class MultirotorDynamics {
 
@@ -33,27 +57,23 @@ class MultirotorDynamics {
         // Parameters from the table below Equation 3
         typedef struct {
 
-            double b;
-            double d;
-            double m;
-            double l;
-            double Ix;
-            double Iy;
-            double Iz;
-            double Jr;
+            double b
+            double d
+            double m
+            double l
+            double Ix
+            double Iy
+            double Iz
+            double Jr
 
-            uint16_t maxrpm;
+            uint16_t maxrpm
 
-        } params_t;
+        } params_t
 
     private:
 
-        // Universal constants
-        static constexpr double g  = 9.80665; // might want to allow this to vary!
-        static constexpr double pi = 3.14159;
-
         // State vector (see Eqn. 11)
-        double _x[12] = {0};
+        double _x[12] = {0}
 
         // State vector position map
         enum {
@@ -69,41 +89,41 @@ class MultirotorDynamics {
             STATE_THETA_DOT,
             STATE_PSI,
             STATE_PSI_DOT
-        };
+        }
 
-        params_t _p;
+        params_t _p
 
-        uint8_t _motorCount = 0;
+        uint8_t _motorCount = 0
 
         // Values computed in Equation 6
-        double _U1 = 0;     // total thrust
-        double _U2 = 0;     // roll thrust right
-        double _U3 = 0;     // pitch thrust forward
-        double _U4 = 0;     // yaw thrust clockwise
-        double _Omega = 0;  // torque clockwise
+        double _U1 = 0     // total thrust
+        double _U2 = 0     // roll thrust right
+        double _U3 = 0     // pitch thrust forward
+        double _U4 = 0     // yaw thrust clockwise
+        double _Omega = 0  // torque clockwise
 
         // Radians per second for each motor
-        double * _omegas = NULL;
+        double * _omegas = NULL
 
         // Inertial-frame acceleration
-        double _inertialAccel[3] = {0};
+        double _inertialAccel[3] = {0}
 
         // Flag for whether we're airborne
-        bool _airborne = false;
+        bool _airborne = false
 
         // Takeoff altitude, for detecting a crash
-        double _zstart;
+        double _zstart
 
         // Support for debugging
-        char _message[200];
+        char _message[200]
 
         // y = Ax + b helper for frame-of-reference conversion methods
         static void dot(double A[3][3], double x[3], double y[3])
         {
-            for (uint8_t j=0; j<3; ++j) {
-                y[j] = 0;
-                for (uint8_t k=0; k<3; ++k) {
-                    y[j] += A[j][k] * x[k];
+            for (uint8_t j=0 j<3 ++j) {
+                y[j] = 0
+                for (uint8_t k=0 k<3 ++k) {
+                    y[j] += A[j][k] * x[k]
                 }
             }
         }
@@ -111,63 +131,63 @@ class MultirotorDynamics {
         // bodyToInertial method optimized for body X=Y=0
         static void bodyZToInertial(double bodyZ, const double rotation[3], double inertial[3])
         {
-            double phi   = rotation[0];
-            double theta = rotation[1];
-            double psi   = rotation[2];
+            double phi   = rotation[0]
+            double theta = rotation[1]
+            double psi   = rotation[2]
 
-            double cph = cos(phi);
-            double sph = sin(phi);
-            double cth = cos(theta);
-            double sth = sin(theta);
-            double cps = cos(psi);
-            double sps = sin(psi);
+            double cph = cos(phi)
+            double sph = sin(phi)
+            double cth = cos(theta)
+            double sth = sin(theta)
+            double cps = cos(psi)
+            double sps = sin(psi)
 
             // This is the rightmost column of the body-to-inertial rotation matrix
             double R[3] = {sph*sps + cph*cps*sth, 
                 cph*sps*sth - cps*sph, 
-                cph*cth};
+                cph*cth}
 
-            for (uint8_t i=0; i<3; ++i) {
-                inertial[i] = bodyZ * R[i];
+            for (uint8_t i=0 i<3 ++i) {
+                inertial[i] = bodyZ * R[i]
             }
         }
 
     protected:
 
         // roll right
-        virtual double u2(double * o) = 0;
+        virtual double u2(double * o) = 0
 
         // pitch forward
-        virtual double u3(double * o) = 0;
+        virtual double u3(double * o) = 0
 
         // yaw cw
-        virtual double u4(double * o) = 0;
+        virtual double u4(double * o) = 0
 
         // motor direction for animation
-        virtual int8_t motorDirection(uint8_t i) = 0;
+        virtual int8_t motorDirection(uint8_t i) = 0
 
          /**
          *  Constructor
          */
         MultirotorDynamics(const params_t & params, const uint8_t motorCount)
         {
-            _motorCount = motorCount;
+            _motorCount = motorCount
 
-            _omegas = new double[motorCount];
+            _omegas = new double[motorCount]
 
-            _p.b = params.b;
-            _p.d = params.d;
-            _p.m = params.m;
-            _p.l = params.l;
-            _p.Ix = params.Ix;
-            _p.Iy = params.Iy;
-            _p.Iz = params.Iz;
-            _p.Jr = params.Jr;
+            _p.b = params.b
+            _p.d = params.d
+            _p.m = params.m
+            _p.l = params.l
+            _p.Ix = params.Ix
+            _p.Iy = params.Iy
+            _p.Iz = params.Iz
+            _p.Jr = params.Jr
 
-            _p.maxrpm = params.maxrpm;
+            _p.maxrpm = params.maxrpm
 
-            for (uint8_t i=0; i<12; ++i) {
-                _x[i] = 0;
+            for (uint8_t i=0 i<12 ++i) {
+                _x[i] = 0
             }
         }
 
@@ -180,61 +200,61 @@ class MultirotorDynamics {
         // Kinematics
         typedef struct {
 
-            double location[3];
-            double rotation[3]; 
+            double location[3]
+            double rotation[3] 
 
-        } pose_t;
+        } pose_t
 
         typedef struct {
 
-            double angularVel[3]; 
-            double bodyAccel[3]; 
-            double bodyVel[3]; 
-            double inertialVel[3]; 
-            double quaternion[4]; 
+            double angularVel[3] 
+            double bodyAccel[3] 
+            double bodyVel[3] 
+            double inertialVel[3] 
+            double quaternion[4] 
 
-            pose_t pose;
+            pose_t pose
 
-        } state_t;
+        } state_t
 
         /**
          *  Destructor
          */
         virtual ~MultirotorDynamics(void)
         {
-            delete _omegas;
+            delete _omegas
         }
 
         /** 
          * Initializes kinematic pose, with flag for whether we're airbone (helps with testing gravity).
          *
-         * @param pose location X,Y,Z; rotation phi,theta,psi
+         * @param pose location X,Y,Z rotation phi,theta,psi
          * @param airborne allows us to start on the ground (default) or in the air (e.g., gravity test)
          */
         void init(const pose_t & pose, bool airborne=false)
         {
             // Initialize state
-            _x[STATE_X]         = pose.location[0];
-            _x[STATE_X_DOT]     = 0;
-            _x[STATE_Y]         = pose.location[1];
-            _x[STATE_Y_DOT]     = 0;
-            _x[STATE_Z]         = pose.location[2];
-            _x[STATE_Z_DOT]     = 0;
-            _x[STATE_PHI]       = pose.rotation[0];
-            _x[STATE_PHI_DOT]   = 0;
-            _x[STATE_THETA]     = pose.rotation[1];
-            _x[STATE_THETA_DOT] = 0;
-            _x[STATE_PSI]       = pose.rotation[2];
-            _x[STATE_PSI_DOT]   = 0;
+            _x[STATE_X]         = pose.location[0]
+            _x[STATE_X_DOT]     = 0
+            _x[STATE_Y]         = pose.location[1]
+            _x[STATE_Y_DOT]     = 0
+            _x[STATE_Z]         = pose.location[2]
+            _x[STATE_Z_DOT]     = 0
+            _x[STATE_PHI]       = pose.rotation[0]
+            _x[STATE_PHI_DOT]   = 0
+            _x[STATE_THETA]     = pose.rotation[1]
+            _x[STATE_THETA_DOT] = 0
+            _x[STATE_PSI]       = pose.rotation[2]
+            _x[STATE_PSI_DOT]   = 0
 
             // Initialize inertial frame acceleration in NED coordinates
-            bodyZToInertial(-g, pose.rotation, _inertialAccel);
+            bodyZToInertial(-g, pose.rotation, _inertialAccel)
 
             // We can start on the ground (default) or in the air
-            _airborne = airborne;
+            _airborne = airborne
 
             // Remember our altitude at takeoff
-            _zstart = pose.location[2];
+            _zstart = pose.location[2]
         }
 
         /** 
@@ -246,23 +266,23 @@ class MultirotorDynamics {
         {
             // Use the current Euler angles to rotate the orthogonal thrust vector into the inertial frame.
             // Negate to use NED.
-            double euler[3] = { _x[6], _x[8], _x[10] };
-            double ned[3] = {0};
-            bodyZToInertial(-_U1/_p.m, euler, ned);
+            double euler[3] = { _x[6], _x[8], _x[10] }
+            double ned[3] = {0}
+            bodyZToInertial(-_U1/_p.m, euler, ned)
 
             // We're airborne once net downward acceleration goes below zero
-            double netz = ned[2] + g;
+            double netz = ned[2] + g
             if (!_airborne) {
-                _airborne = netz < 0;
+                _airborne = netz < 0
             }
 
             // Once airborne, we can update dynamics
             if (_airborne) {
 
                 // Make some useful abbreviations
-                double phidot = _x[STATE_PHI_DOT];
-                double thedot = _x[STATE_THETA_DOT];
-                double psidot = _x[STATE_PSI_DOT];
+                double phidot = _x[STATE_PHI_DOT]
+                double thedot = _x[STATE_THETA_DOT]
+                double psidot = _x[STATE_PSI_DOT]
 
                 double dxdt[12] = {
 
@@ -279,17 +299,17 @@ class MultirotorDynamics {
                     /* theta'' */ -(psidot*phidot*(_p.Iz-_p.Ix)/_p.Iy + _p.Jr/_p.Iy*phidot*_Omega + _p.l/_p.Iy*_U3), 
                     /* psi'    */ psidot,
                     /* psi''   */ thedot*phidot*(_p.Ix-_p.Iy)/_p.Iz   + _p.l/_p.Iz*_U4,
-                };
+                }
 
                 // Compute state as first temporal integral of first temporal derivative
-                for (uint8_t i=0; i<12; ++i) {
-                    _x[i] += dt * dxdt[i];
+                for (uint8_t i=0 i<12 ++i) {
+                    _x[i] += dt * dxdt[i]
                 }
 
                 // Once airborne, inertial-frame acceleration is same as NED acceleration
-                _inertialAccel[0] = ned[0];
-                _inertialAccel[1] = ned[1];
-                _inertialAccel[2] = ned[2];
+                _inertialAccel[0] = ned[0]
+                _inertialAccel[1] = ned[1]
+                _inertialAccel[2] = ned[2]
             }
         }
 
@@ -301,24 +321,24 @@ class MultirotorDynamics {
         void setMotors(double * motorvals) 
         {
             // Convert the  motor values to radians per second
-            for (unsigned int i=0; i<_motorCount; ++i) {
-                _omegas[i] = motorvals[i] * _p.maxrpm * pi / 30;
+            for (unsigned int i=0 i<_motorCount ++i) {
+                _omegas[i] = motorvals[i] * _p.maxrpm * pi / 30
             }
 
             // Compute overall torque from omegas before squaring
-            _Omega = u4(_omegas);
+            _Omega = u4(_omegas)
 
             // Overall thrust is sum of squared omegas
-            _U1 = 0;
-            for (unsigned int i=0; i<_motorCount; ++i) {
-                _omegas[i] *= _omegas[i];
-                _U1 +=  _p.b * _omegas[i];
+            _U1 = 0
+            for (unsigned int i=0 i<_motorCount ++i) {
+                _omegas[i] *= _omegas[i]
+                _U1 +=  _p.b * _omegas[i]
             }
 
             // Use the squared Omegas to implement the rest of Eqn. 6
-            _U2 = _p.b * u2(_omegas);
-            _U3 = _p.b * u3(_omegas);
-            _U4 = _p.d * u4(_omegas);
+            _U2 = _p.b * u2(_omegas)
+            _U3 = _p.b * u3(_omegas)
+            _U4 = _p.d * u4(_omegas)
         }
 
         /*
@@ -332,22 +352,22 @@ class MultirotorDynamics {
         {
 
             // Get most values directly from state vector
-            for (uint8_t i=0; i<3; ++i) {
-                uint8_t ii = 2 * i;
-                state.angularVel[i]    = _x[STATE_PHI_DOT+ii];
-                state.inertialVel[i]   = _x[STATE_X_DOT+ii];
-                state.pose.rotation[i] = _x[STATE_PHI+ii];
-                state.pose.location[i] = _x[STATE_X+ii];
+            for (uint8_t i=0 i<3 ++i) {
+                uint8_t ii = 2 * i
+                state.angularVel[i]    = _x[STATE_PHI_DOT+ii]
+                state.inertialVel[i]   = _x[STATE_X_DOT+ii]
+                state.pose.rotation[i] = _x[STATE_PHI+ii]
+                state.pose.location[i] = _x[STATE_X+ii]
             }
 
             // Convert inertial acceleration and velocity to body frame
-            inertialToBody(_inertialAccel, state.pose.rotation, state.bodyAccel);
+            inertialToBody(_inertialAccel, state.pose.rotation, state.bodyAccel)
 
             // Convert Euler angles to quaternion
-            eulerToQuaternion(state.pose.rotation, state.quaternion);
+            eulerToQuaternion(state.pose.rotation, state.quaternion)
 
             // If we're airborne, we've crashed if we fall below ground level
-            return _airborne ? (state.pose.location[2] > _zstart) : false;
+            return _airborne ? (state.pose.location[2] > _zstart) : false
         }
 
         /**
@@ -357,7 +377,7 @@ class MultirotorDynamics {
          */
         char * getMessage(void)
         {
-            return _message;
+            return _message
         }
 
         /**
@@ -368,42 +388,42 @@ class MultirotorDynamics {
 
         static void bodyToInertial(double body[3], const double rotation[3], double inertial[3])
         {
-            double phi   = rotation[0];
-            double theta = rotation[1];
-            double psi   = rotation[2];
+            double phi   = rotation[0]
+            double theta = rotation[1]
+            double psi   = rotation[2]
 
-            double cph = cos(phi);
-            double sph = sin(phi);
-            double cth = cos(theta);
-            double sth = sin(theta);
-            double cps = cos(psi);
-            double sps = sin(psi);
+            double cph = cos(phi)
+            double sph = sin(phi)
+            double cth = cos(theta)
+            double sth = sin(theta)
+            double cps = cos(psi)
+            double sps = sin(psi)
 
             double R[3][3] = { {cps*cth,  cps*sph*sth - cph*sps,  sph*sps + cph*cps*sth}, 
                                {cth*sps,  cph*cps + sph*sps*sth,  cph*sps*sth - cps*sph}, 
-                               {-sth,     cth*sph,                cph*cth}               };
+                               {-sth,     cth*sph,                cph*cth}               }
 
-            dot(R, body, inertial);
+            dot(R, body, inertial)
         }
 
         static void inertialToBody(double inertial[3], const double rotation[3], double body[3])
         {
-            double phi   = rotation[0];
-            double theta = rotation[1];
-            double psi   = rotation[2];
+            double phi   = rotation[0]
+            double theta = rotation[1]
+            double psi   = rotation[2]
 
-            double cph = cos(phi);
-            double sph = sin(phi);
-            double cth = cos(theta);
-            double sth = sin(theta);
-            double cps = cos(psi);
-            double sps = sin(psi);
+            double cph = cos(phi)
+            double sph = sin(phi)
+            double cth = cos(theta)
+            double sth = sin(theta)
+            double cps = cos(psi)
+            double sps = sin(psi)
 
             double R[3][3] = { {cps*cth,                cth*sps,                   -sth}, 
                                {cps*sph*sth - cph*sps,  cph*cps + sph*sps*sth,  cth*sph}, 
-                               {sph*sps + cph*cps*sth,  cph*sps*sth - cps*sph,  cph*cth} };
+                               {sph*sps + cph*cps*sth,  cph*sps*sth - cps*sph,  cph*cth} }
 
-            dot(R, inertial, body);
+            dot(R, inertial, body)
         }
 
         /**
@@ -416,23 +436,23 @@ class MultirotorDynamics {
         static void eulerToQuaternion(const double eulerAngles[3], double quaternion[4])
         {
             // Convenient renaming
-            double phi = eulerAngles[0] / 2;
-            double the = eulerAngles[1] / 2;
-            double psi = eulerAngles[2] / 2;
+            double phi = eulerAngles[0] / 2
+            double the = eulerAngles[1] / 2
+            double psi = eulerAngles[2] / 2
 
             // Pre-computation
-            double cph = cos(phi);
-            double cth = cos(the);
-            double cps = cos(psi);
-            double sph = sin(phi);
-            double sth = sin(the);
-            double sps = sin(psi);
+            double cph = cos(phi)
+            double cth = cos(the)
+            double cps = cos(psi)
+            double sph = sin(phi)
+            double sth = sin(the)
+            double sps = sin(psi)
 
             // Conversion
-            quaternion[0] =  cph * cth * cps + sph * sth * sps;
-            quaternion[1] =  cph * sth * sps - sph * cth * cps ;
-            quaternion[2] = -cph * sth * cps - sph * cth * sps;
-            quaternion[3] =  cph * cth * sps - sph * sth * cps;
+            quaternion[0] =  cph * cth * cps + sph * sth * sps
+            quaternion[1] =  cph * sth * sps - sph * cth * cps 
+            quaternion[2] = -cph * sth * cps - sph * cth * sps
+            quaternion[3] =  cph * cth * sps - sph * sth * cps
         }
 
         /**
@@ -440,13 +460,13 @@ class MultirotorDynamics {
          */
         uint8_t motorCount(void)
         {
-            return _motorCount;
+            return _motorCount
         }
 
         /**
          * Factory method
          */
-        static MultirotorDynamics * create(void);
+        static MultirotorDynamics * create(void)
 
-}; // class MultirotorDynamics
+} // class MultirotorDynamics
 '''
