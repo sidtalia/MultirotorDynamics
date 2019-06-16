751c7e5d4022e17b929f796b5a4ba7d1d80de827
Your Name <you@example.com>
2019-06-16 17:15:08 +0300

    =Moar code

--- multirotor_dynamics.py
+++ multirotor_dynamics.py
@@ -11,11 +11,11 @@
       pages     = {4393--4398},
       year      = {2004},
       crossref  = {DBLP:conf/icra/2004},
-      url       = {https://doi.org/10.1109/ROBOT.2004.1302409},
+      url       = {https:#doi.org/10.1109/ROBOT.2004.1302409},
       doi       = {10.1109/ROBOT.2004.1302409},
       timestamp = {Sun, 04 Jun 2017 01:00:00 +0200},
-      biburl    = {https://dblp.org/rec/bib/conf/icra/BouabdallahMS04},
-      bibsource = {dblp computer science bibliography, https://dblp.org}
+      biburl    = {https:#dblp.org/rec/bib/conf/icra/BouabdallahMS04},
+      bibsource = {dblp computer science bibliography, https:#dblp.org}
     }
   
   Copyright (C) 2019 Simon D. Levy
@@ -27,7 +27,20 @@ import numpy as np
 
 class MultirotorDynamics(object):
 
-    # Might want to allow G to vary based on altitude
+    STATE_X         = 0
+    STATE_X_DOT     = 1
+    STATE_Y         = 2
+    STATE_Y_DOT     = 3
+    STATE_Z         = 4
+    STATE_Z_DOT     = 5
+    STATE_PHI       = 6
+    STATE_PHI_DOT   = 7
+    STATE_THETA     = 8
+    STATE_THETA_DOT = 9
+    STATE_PSI       = 10
+    STATE_PSI_DOT   = 11
+
+     # Might want to allow G to vary based on altitude
     g  = 9.80665 
 
     def __init(self, motorCount, b, d, m, l, Ix, Iy, Iz, Jr, maxrpm):
@@ -49,75 +62,27 @@ class MultirotorDynamics(object):
 
         self.maxrpm = maxrpm
 
+        # Values computed in Equation 6
+        self.U1 = 0     # total thrust
+        self.U2 = 0     # roll thrust right
+        self.U3 = 0     # pitch thrust forward
+        self.U4 = 0     # yaw thrust clockwise
+        self.Omega = 0  # torque clockwise
+
+        # Radians per second for each motor
+        self.omegas = np.zeros(motorCount)
+
+        # Inertial-frame acceleration
+        self.inertialAccel = np.zeros(3)
+
+        # Flag for whether we're airborne
+        self.airborne = False
+
+        # Takeoff altitude, for detecting a crash
+        self.zstart = 0
+        
 '''
-class MultirotorDynamics {
-
-    public:
-
-        // Parameters from the table below Equation 3
-        typedef struct {
-
-            double b
-            double d
-            double m
-            double l
-            double Ix
-            double Iy
-            double Iz
-            double Jr
-
-            uint16_t maxrpm
-
-        } params_t
-
-    private:
-
-        // State vector (see Eqn. 11)
-        double _x[12] = {0}
-
-        // State vector position map
-        enum {
-            STATE_X, 
-            STATE_X_DOT, 
-            STATE_Y,
-            STATE_Y_DOT,
-            STATE_Z,
-            STATE_Z_DOT,
-            STATE_PHI,
-            STATE_PHI_DOT,
-            STATE_THETA,
-            STATE_THETA_DOT,
-            STATE_PSI,
-            STATE_PSI_DOT
-        }
-
-        params_t _p
-
-        uint8_t _motorCount = 0
-
-        // Values computed in Equation 6
-        double _U1 = 0     // total thrust
-        double _U2 = 0     // roll thrust right
-        double _U3 = 0     // pitch thrust forward
-        double _U4 = 0     // yaw thrust clockwise
-        double _Omega = 0  // torque clockwise
-
-        // Radians per second for each motor
-        double * _omegas = NULL
-
-        // Inertial-frame acceleration
-        double _inertialAccel[3] = {0}
-
-        // Flag for whether we're airborne
-        bool _airborne = false
-
-        // Takeoff altitude, for detecting a crash
-        double _zstart
-
-        // Support for debugging
-        char _message[200]
-
-        // y = Ax + b helper for frame-of-reference conversion methods
+        # y = Ax + b helper for frame-of-reference conversion methods
         static void dot(double A[3][3], double x[3], double y[3])
         {
             for (uint8_t j=0 j<3 ++j) {
@@ -128,7 +93,7 @@ class MultirotorDynamics {
             }
         }
 
-        // bodyToInertial method optimized for body X=Y=0
+        # bodyToInertial method optimized for body X=Y=0
         static void bodyZToInertial(double bodyZ, const double rotation[3], double inertial[3])
         {
             double phi   = rotation[0]
@@ -142,7 +107,7 @@ class MultirotorDynamics {
             double cps = cos(psi)
             double sps = sin(psi)
 
-            // This is the rightmost column of the body-to-inertial rotation matrix
+            # This is the rightmost column of the body-to-inertial rotation matrix
             double R[3] = {sph*sps + cph*cps*sth, 
                 cph*sps*sth - cps*sph, 
                 cph*cth}
@@ -154,16 +119,16 @@ class MultirotorDynamics {
 
     protected:
 
-        // roll right
+        # roll right
         virtual double u2(double * o) = 0
 
-        // pitch forward
+        # pitch forward
         virtual double u3(double * o) = 0
 
-        // yaw cw
+        # yaw cw
         virtual double u4(double * o) = 0
 
-        // motor direction for animation
+        # motor direction for animation
         virtual int8_t motorDirection(uint8_t i) = 0
 
          /**
@@ -197,7 +162,7 @@ class MultirotorDynamics {
          * Exported state representations
          */
 
-        // Kinematics
+        # Kinematics
         typedef struct {
 
             double location[3]
@@ -233,7 +198,7 @@ class MultirotorDynamics {
          */
         void init(const pose_t & pose, bool airborne=false)
         {
-            // Initialize state
+            # Initialize state
             _x[STATE_X]         = pose.location[0]
             _x[STATE_X_DOT]     = 0
             _x[STATE_Y]         = pose.location[1]
@@ -247,13 +212,13 @@ class MultirotorDynamics {
             _x[STATE_PSI]       = pose.rotation[2]
             _x[STATE_PSI_DOT]   = 0
 
-            // Initialize inertial frame acceleration in NED coordinates
+            # Initialize inertial frame acceleration in NED coordinates
             bodyZToInertial(-g, pose.rotation, _inertialAccel)
 
-            // We can start on the ground (default) or in the air
+            # We can start on the ground (default) or in the air
             _airborne = airborne
 
-            // Remember our altitude at takeoff
+            # Remember our altitude at takeoff
             _zstart = pose.location[2]
         }
 
@@ -264,29 +229,29 @@ class MultirotorDynamics {
          */
         void update(double dt)
         {
-            // Use the current Euler angles to rotate the orthogonal thrust vector into the inertial frame.
-            // Negate to use NED.
+            # Use the current Euler angles to rotate the orthogonal thrust vector into the inertial frame.
+            # Negate to use NED.
             double euler[3] = { _x[6], _x[8], _x[10] }
             double ned[3] = {0}
             bodyZToInertial(-_U1/_p.m, euler, ned)
 
-            // We're airborne once net downward acceleration goes below zero
+            # We're airborne once net downward acceleration goes below zero
             double netz = ned[2] + g
             if (!_airborne) {
                 _airborne = netz < 0
             }
 
-            // Once airborne, we can update dynamics
+            # Once airborne, we can update dynamics
             if (_airborne) {
 
-                // Make some useful abbreviations
+                # Make some useful abbreviations
                 double phidot = _x[STATE_PHI_DOT]
                 double thedot = _x[STATE_THETA_DOT]
                 double psidot = _x[STATE_PSI_DOT]
 
                 double dxdt[12] = {
 
-                    // Equation 12: compute temporal first derivative of state.
+                    # Equation 12: compute temporal first derivative of state.
                     /* x'      */ _x[STATE_X_DOT],
                     /* x''     */ ned[0],
                     /* y'      */ _x[STATE_Y_DOT],
@@ -301,12 +266,12 @@ class MultirotorDynamics {
                     /* psi''   */ thedot*phidot*(_p.Ix-_p.Iy)/_p.Iz   + _p.l/_p.Iz*_U4,
                 }
 
-                // Compute state as first temporal integral of first temporal derivative
+                # Compute state as first temporal integral of first temporal derivative
                 for (uint8_t i=0 i<12 ++i) {
                     _x[i] += dt * dxdt[i]
                 }
 
-                // Once airborne, inertial-frame acceleration is same as NED acceleration
+                # Once airborne, inertial-frame acceleration is same as NED acceleration
                 _inertialAccel[0] = ned[0]
                 _inertialAccel[1] = ned[1]
                 _inertialAccel[2] = ned[2]
@@ -320,22 +285,22 @@ class MultirotorDynamics {
          */
         void setMotors(double * motorvals) 
         {
-            // Convert the  motor values to radians per second
+            # Convert the  motor values to radians per second
             for (unsigned int i=0 i<_motorCount ++i) {
                 _omegas[i] = motorvals[i] * _p.maxrpm * pi / 30
             }
 
-            // Compute overall torque from omegas before squaring
+            # Compute overall torque from omegas before squaring
             _Omega = u4(_omegas)
 
-            // Overall thrust is sum of squared omegas
+            # Overall thrust is sum of squared omegas
             _U1 = 0
             for (unsigned int i=0 i<_motorCount ++i) {
                 _omegas[i] *= _omegas[i]
                 _U1 +=  _p.b * _omegas[i]
             }
 
-            // Use the squared Omegas to implement the rest of Eqn. 6
+            # Use the squared Omegas to implement the rest of Eqn. 6
             _U2 = _p.b * u2(_omegas)
             _U3 = _p.b * u3(_omegas)
             _U4 = _p.d * u4(_omegas)
@@ -351,7 +316,7 @@ class MultirotorDynamics {
         bool getState(state_t & state)
         {
 
-            // Get most values directly from state vector
+            # Get most values directly from state vector
             for (uint8_t i=0 i<3 ++i) {
                 uint8_t ii = 2 * i
                 state.angularVel[i]    = _x[STATE_PHI_DOT+ii]
@@ -360,13 +325,13 @@ class MultirotorDynamics {
                 state.pose.location[i] = _x[STATE_X+ii]
             }
 
-            // Convert inertial acceleration and velocity to body frame
+            # Convert inertial acceleration and velocity to body frame
             inertialToBody(_inertialAccel, state.pose.rotation, state.bodyAccel)
 
-            // Convert Euler angles to quaternion
+            # Convert Euler angles to quaternion
             eulerToQuaternion(state.pose.rotation, state.quaternion)
 
-            // If we're airborne, we've crashed if we fall below ground level
+            # If we're airborne, we've crashed if we fall below ground level
             return _airborne ? (state.pose.location[2] > _zstart) : false
         }
 
@@ -383,7 +348,7 @@ class MultirotorDynamics {
         /**
          *  Frame-of-reference conversion routines.
          *
-         *  See Section 5 of http://www.chrobotics.com/library/understanding-euler-angles
+         *  See Section 5 of http:#www.chrobotics.com/library/understanding-euler-angles
          */
 
         static void bodyToInertial(double body[3], const double rotation[3], double inertial[3])
@@ -435,12 +400,12 @@ class MultirotorDynamics {
 
         static void eulerToQuaternion(const double eulerAngles[3], double quaternion[4])
         {
-            // Convenient renaming
+            # Convenient renaming
             double phi = eulerAngles[0] / 2
             double the = eulerAngles[1] / 2
             double psi = eulerAngles[2] / 2
 
-            // Pre-computation
+            # Pre-computation
             double cph = cos(phi)
             double cth = cos(the)
             double cps = cos(psi)
@@ -448,7 +413,7 @@ class MultirotorDynamics {
             double sth = sin(the)
             double sps = sin(psi)
 
-            // Conversion
+            # Conversion
             quaternion[0] =  cph * cth * cps + sph * sth * sps
             quaternion[1] =  cph * sth * sps - sph * cth * cps 
             quaternion[2] = -cph * sth * cps - sph * cth * sps
@@ -468,5 +433,5 @@ class MultirotorDynamics {
          */
         static MultirotorDynamics * create(void)
 
-} // class MultirotorDynamics
+} # class MultirotorDynamics
 '''
