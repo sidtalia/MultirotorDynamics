a83e9fc0d06480a06c9bb918d67a48c55a66bd84
unknown <simondlevy>
2019-06-17 20:52:15 +0300

    More demo work

--- quadx_demo.py
+++ quadx_demo.py
@@ -1,4 +1,5 @@
 '''
+
   Simple demo of multirotor dynamics Quad-X layout with ArduPilot
   motor-numbering convention:
 
@@ -11,7 +12,10 @@
    Copyright (C) 2019 Simon D. Levy
  
   MIT License
+
 '''
+
+from multirotor_dynamics import Dynamics
 
 if __name__ == '__main__':
 
