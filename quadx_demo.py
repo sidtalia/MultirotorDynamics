e74c0d8c2401c270dda6489883b209913c8cdbfb
unknown <simondlevy>
2019-06-17 21:51:50 +0300

    Implemented u2(), u3(), u4() methods

--- quadx_demo.py
+++ quadx_demo.py
@@ -23,6 +23,24 @@ class QuadXAP(Dynamics):
 
         Dynamics.__init__(self, 4, b, d, m, l, Ix, Iy, Iz, Jr, maxrpm)
 
+    # roll right
+    def u2(o):
+    
+        return (o[1] + o[2]) - (o[0] + o[3])
+    
+
+    # pitch forward
+    def u3(o):
+    
+        return (o[1] + o[3]) - (o[0] + o[2])
+    
+
+    # yaw cw
+    def u4(o):
+    
+        return (o[0] + o[1]) - (o[2] + o[3])
+
+
 if __name__ == '__main__':
 
 
