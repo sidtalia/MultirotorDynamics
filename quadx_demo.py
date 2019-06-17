6854e1def8528d96077e22ce1c165fdcea5fbc5b
simondlevy <simon.d.levy@gmail.com>
2019-06-17 13:29:27 +0300

    Begin demo

---
+++ quadx_demo.py
@@ -0,0 +1,18 @@
+'''
+  Simple demo of multirotor dynamics Quad-X layout with ArduPilot
+  motor-numbering convention:
+
+    3cw   1ccw
+       \ /
+        X
+       / \
+    2ccw  4cw
+
+   Copyright (C) 2019 Simon D. Levy
+ 
+  MIT License
+'''
+
+if __name__ == '__main__':
+
+    print('okay')
