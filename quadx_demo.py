bf69170780c3cf5949383b2504737cfbd4c817b1
unknown <simondlevy>
2019-06-17 21:47:24 +0300

    More example code

--- quadx_demo.py
+++ quadx_demo.py
@@ -9,14 +9,34 @@
        / \
     2ccw  4cw
 
-   Copyright (C) 2019 Simon D. Levy
- 
+  Copyright (C) 2019 Simon D. Levy
+
   MIT License
 
 '''
 
 from multirotor_dynamics import Dynamics
 
+class QuadXAP(Dynamics):
+
+    def __init__(self, b, d, m, l, Ix, Iy, Iz, Jr, maxrpm):
+
+        Dynamics.__init__(self, 4, b, d, m, l, Ix, Iy, Iz, Jr, maxrpm)
+
 if __name__ == '__main__':
 
-    print('okay')
+
+    # Amir's calculations
+    b  = 5.30216718361085E-05,   
+    d  = 2.23656692806239E-06,   
+    m  = 16.47,                  
+    l  = 0.6,                    
+    Ix = 2,                      
+    Iy = 2,                      
+    Iz = 3,                      
+    Jr = 3.08013E-04,            
+
+    # estimated
+    maxrpm = 15000
+
+    quad = QuadXAP(b, d, m, l, Ix, Iy, Iz, Jr, maxrpm)
